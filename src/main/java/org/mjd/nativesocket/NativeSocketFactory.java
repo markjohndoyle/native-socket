07d0929169c9f58b218c38a7a02a80d52871561f
Mark Doyle <markjohndoyle@googlemail.com>
2017-12-01 22:58:56 +0100

    add SocketChannel support to Factory

--- src/main/java/org/mjd/nativesocket/NativeSocketFactory.java
+++ src/main/java/org/mjd/nativesocket/NativeSocketFactory.java
@@ -1,6 +1,7 @@
 package org.mjd.nativesocket;
 
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
 
 import com.sun.jna.Platform;
 import org.mjd.nativesocket.internal.LinuxSocket;
@@ -19,9 +20,13 @@ public final class NativeSocketFactory
     }
 
     /**
+     * Creates a {@link NativeSocket} from a given {@link Socket}
+     * 
      * @param socket
      *            the Java {@link Socket} to create a Native socket from
      * @return new {@link NativeSocket} for this Platform
+     * @see NativeSocket
+     * @see Socket
      */
     public static NativeSocket createFrom(Socket socket)
     {
@@ -31,5 +36,19 @@ public final class NativeSocketFactory
         }
         throw new IllegalStateException("No Native socket implementation for this platform: " +
                         System.getProperty("os.name"));
+    }
+
+    /**
+     * Creates a {@link NativeSocket} from a given {@link SocketChannel}
+     * 
+     * @param socketChannel
+     *            the Java {@link SocketChannel} to create a Native socket from
+     * @return new {@link NativeSocket} for this Platform
+     * @see NativeSocket
+     * @see Socket
+     */
+    public static NativeSocket createFrom(SocketChannel socketChannel)
+    {
+        return createFrom(socketChannel.socket());
     }
 }
