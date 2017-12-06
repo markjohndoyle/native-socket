c4acd00d19bfdb0df0e063d924b42bc3fe74f963
Unknown <markjohndoyle@googlemail.com>
2017-12-06 15:56:57 +0100

    add javadocs for CBool

--- src/main/java/org/mjd/nativesocket/internal/CBool.java
+++ src/main/java/org/mjd/nativesocket/internal/CBool.java
@@ -4,6 +4,10 @@ import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
 
 
+/**
+ * Java mapping for C boolean
+ *
+ */
 public enum CBool
 {
      /** False C boolean */
@@ -11,7 +15,9 @@ public enum CBool
      /** True C boolean */
      TRUE(1);
 
+    /** C true as int pointer */
     public static final Pointer TRUE_PTR = new IntByReference(TRUE.asCInt()).getPointer();
+    /** C false as int pointer */
     public static final Pointer FALSE_PTR = new IntByReference(FALSE.asCInt()).getPointer();
 
     private final int cIntRep;
@@ -25,16 +31,29 @@ public enum CBool
         cIntRep = integerRep;
     }
 
+    /**
+     * @return this boolean as a C integer
+     */
     public int asCInt()
     {
         return cIntRep;
     }
     
+    /**
+     * @return this booleans as a java boolean.
+     */
     public boolean toBoolean()
     {
         return cIntRep != 0;
     }
     
+    /**
+     * Create a {@link CBool} from an int.
+     * 
+     * @param cIntRep
+     *            the c integer representation of the boolean.
+     * @return new CBool representing cIntRep
+     */
     public static CBool fromInt(int cIntRep)
     {
         return values()[cIntRep];
